d6802402c9e6de66dc1539a1dbd695e2c671b34f
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-08-22 22:02:23 -0500

    more cleanup around cli

--- lib/redcat/cli.rb
+++ lib/redcat/cli.rb
@@ -4,16 +4,15 @@ module Redcat
   class CLI
     def run
       opts = {}
-      opts[:redis_url] = ENV['REDIS_URL']
+      opts[:redis_url] = ENV['REDIS_URL'] || 'redis://localhost:6379'
 
       ARGV.options do |o|
-        o.banner = 'Usage: redcat [options] <file>'
+        o.banner = 'Usage: redcat [options] -l <list> [file]'
         o.on('-u', '--url URL', 'redis url') { |url| opts[:redis_url] = url }
         o.on('-l', '--list LIST', 'redis list') { |list| opts[:redis_list] = list }
         o.parse!
 
-        abort('Error: -u not specified or REDIS_URL not set in ENV') unless opts[:redis_url]
-        abort('Error: -l not specified') unless opts[:redis_list]
+        abort("Error: -l not specified\n\n#{o}") unless opts[:redis_list]
         
         file = ARGV.shift
         io = file.nil? ? ARGF : File.open(file)
